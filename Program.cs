de979faa1ba938369d8e16dee42737b849e2011b
Leandro Vilar <leandrovilar@outlook.com>
2025-04-25 10:52:48 -0300

    Resolucao do Poo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        //Teste com Nokia
+         Console.WriteLine("Smartphone Nokia");
+        Nokia nokia = new Nokia("123456789", "Nokia 1234", "111111111", 32);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine("\n");
+
+        //Teste com iPhone
+        Console.WriteLine("Smartphone iPhone");
+        Iphone iphone = new Iphone("987654321", "iPhone 15", "222222222", 256);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Telegram");
+
+    }
+}
